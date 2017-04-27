043f00bcb409869863d354bb8d0aa78cfc413167
Simen Brekken <simen@unfold.no>
2017-04-27 14:18:52 +0200

    Change directory

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -13,13 +13,16 @@ list_dependencies() {
 
 run_if_present() {
   local script_name=${1:-}
-  local has_script=$(read_json "$BUILD_DIR/package.json" ".scripts[\"$script_name\"]")
+  local build_dir=${2:-$BUILD_DIR}
+  local has_script=$(read_json "$build_dir/package.json" ".scripts[\"$script_name\"]")
   if [ -n "$has_script" ]; then
     if $YARN; then
       echo "Running $script_name (yarn)"
+      cd "$build_dir"
       yarn run "$script_name"
     else
       echo "Running $script_name"
+      cd "$build_dir"
       npm run "$script_name" --if-present
     fi
   fi
@@ -31,7 +34,7 @@ yarn_node_modules() {
   echo "Installing node modules (yarn.lock)"
   cd "$build_dir"
   yarn install --pure-lockfile --ignore-engines 2>&1
-  run_if_present 'heroku-postbuild'
+  run_if_present 'heroku-postbuild' $build_dir
 }
 
 npm_node_modules() {
@@ -45,7 +48,7 @@ npm_node_modules() {
     else
       echo "Installing node modules (package.json)"
     fi
-    npm install --silent --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
   fi
